7d55c2d411ba981ec713527fbcc95b09761ff714
kfir marouani <kfir.marouani@my6sense.com>
2018-03-26 15:02:42 +0300

    Adding getAllFuncs to support CJS and ES6 functions

--- src/aspectlee.js
+++ src/aspectlee.js
@@ -4,11 +4,29 @@ export {State} from './state.js'
 
 class AspectleeClass {
 
+    getAllFuncs(obj) {
+        var props = [];
+
+        do {
+            // excluding inner object functions
+            if (Object.getOwnPropertyNames(obj).indexOf('hasOwnProperty') >-1 ) {
+                break;
+            } else {
+                props = props.concat(Object.getOwnPropertyNames(obj));
+            }
+
+        } while (obj = Object.getPrototypeOf(obj));
+
+        // return props.sort().filter(function(e, i, arr) {
+        //     if (e!=arr[i+1] && typeof obj[e] == 'function') return true;
+        // });
+
+        return props;
+    }
+
     constructor(obj, printCallback) {
 
-        const props = Object.getPrototypeOf(obj);
-
-        Object.getOwnPropertyNames(props).forEach( (funcName) => {
+        this.getAllFuncs(obj).forEach( (funcName) => {
 
             if (typeof obj[funcName] === 'function'){
                 const funcInterseptor = new Interceptor(obj[funcName], funcName, printCallback);
